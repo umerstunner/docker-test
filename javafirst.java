1a566ae12bfa7effbdd4053a351b76b7b376412a
umerstunner <113825631+umerstunner@users.noreply.github.com>
2024-11-22 11:10:16 +0500

    Update javafirst.java

--- javafirst.java
+++ javafirst.java
@@ -1,5 +1,6 @@
 public class javafirst {
     public static void main(String[] args) {
         System.out.println("My Roll Number is: FA21-BCS-092. this is a secong change");
+        System.out.println("My Roll Number is: FA21-BCS-092. this is a secong change");
     }
 }
